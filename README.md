a6fd7dc76b9f727b789bbe4693d3162609e21fc3
agung <agungprsty423@gmail.com>
2022-02-08 00:01:11 +0700

    add content envelope

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 ## Assets bundler
 
 ### Requirement : 
-- webpack ^5, please refer to : https://webpack.js.org/concepts/ for installation
+- Bootstrap ^5, please refer to : https://getbootstrap.com/ for include to vendors
+- Jquery ^3.5, please refer to : https://jquery.com/ for include to vendors
+- GSAP ^3.2, please refer to : https://greensock.com/ for include to vendors
+- Webpack ^5, please refer to : https://webpack.js.org/concepts/ for installation
 
 ### Setup 
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,60 @@
 import style from './sass/style.scss';
 
-$(document).ready(function(){
-    $("#open").click(function(){
-      alert("Open");
-    });
+let envelope = $('.envelope');
+let close = $('.close');
+let t1 = gsap.timeline({ paused: true });             // GreenSock Animations Platform
+let flap = CSSRulePlugin.getRule(".envelope:before"); 
+
+t1.to(flap, { 
+  duration: 0.5, 
+  cssRule: {
+    rotateX: 180
+  }
+})
+.set(flap, {
+  cssRule: {
+    zIndex: 10
+  }
+})
+.to('.letter', {
+  translateY: -200,
+  duration: 0.9, 
+  ease: "back.inOut(1.5)"
+})
+.set('.letter', {
+  zIndex: 40
+})
+.to('.letter', {
+  duration: .7,  
+  ease: "back.out(.4)",
+  translateY: -5,
+  translateZ: 250
+});
+
+let t2 = gsap.timeline({ paused: true }); 
+t2.to('.shadow', {
+  delay: 1.4,
+  width: 450,
+  boxShadow: "-75px 150px 10px 5px #eeeef3",
+  ease: "back.out(.2)",
+  duration: .7
+}); 
+  
+function openCard(e) {
+  t1.play();
+  t2.play();
+}
+
+function closeCard(e) {
+  t1.reverse();
+  t2.reverse();
+}
+
+$(envelope).click(function() {
+  openCard();
+});
+
+$(close).click(function(e) {
+   e.stopPropagation();
+   closeCard();
 });
